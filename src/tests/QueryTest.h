8a7ba44574e65faf6b9c9cc3c6c8f8a38546bd05
pabloDeputter <pablo.deputter@student.uantwerpen.be>
2022-01-27 16:57:55 +0100

    test via system

--- src/tests/QueryTest.h
+++ src/tests/QueryTest.h
@@ -16,22 +16,14 @@
 #define RATSQL_PARSING_QUERYTEST_H
 
 #include "Test.h"
-
-// TODO - doen via Cedric zijn klasse?
-#include "../parser/EarleyParser.h"
-#include "../parser/Lexer.h"
-#include "../datastructures/CFG.h"
+#include "../system/System.h"
 
 #include <memory>
 
 class QueryTest : public Test {
 
 public:
-    QueryTest() : Test() {
-        mCFG = std::make_shared<CFG>("input/grammar.json");
-        mLexer = std::make_unique<Lexer>(mCFG->getAliasMap(), mCFG->getAliases());
-        mParser = std::make_unique<EarleyParser>(mCFG);
-    }
+    QueryTest() : Test() { mSystem = std::make_unique<System>(); }
 
     void run() override;
 
@@ -48,11 +40,7 @@ public:
     virtual ~QueryTest() = default;
 
 private:
-    std::shared_ptr<CFG> mCFG;
-    std::unique_ptr<Lexer> mLexer;
-    std::unique_ptr<EarleyParser> mParser;
-
-    std::string translate(const std::string& s);
+    std::unique_ptr<System> mSystem;
 
     std::string createMessage(const std::string& given, const std::string& exp, const std::string& test) const;
 };
